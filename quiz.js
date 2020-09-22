e7586a5ac3b9ebdcdad5e652f741939941a9b20f
Pikachu pikachu <70988580+learndaily1@users.noreply.github.com>
2020-09-22 16:14:58 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,12 @@
+function visibility1() {
+        document.querySelectorAll("div")[0].classList.add = "visibility1";
+  
+    document.querySelector(".visibility1").style.display = "none";
+
+document.querySelector(".visibility").style.display="block";
+ }
+function visibility() {
+      document.querySelector(".visibility").style.display = "none";
+  document.querySelector(".visibility1").style.display = "block";
+
+}
